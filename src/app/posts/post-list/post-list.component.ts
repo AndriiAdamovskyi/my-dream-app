cdc92b5fefdc0cd0a07c1f1cc56c02282a992278
Andrii Adamovskyi <timon9551@gmail.com>
2022-09-29 13:50:38 +0300

    fix mistype

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,7 +23,7 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.postsService.getPosts();
-    this.postsSub = this.postsService.getPostsUpdateListener()
+    this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
         this.posts = posts;
       });
