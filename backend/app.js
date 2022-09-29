46199a0a1dfb720ef2aae0f7651c8229563bd7dc
Andrii Adamovskyi <timon9551@gmail.com>
2022-09-29 18:58:17 +0300

    removed dummy posts on server side, fetch it from db using find()

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,16 @@ const Post = require("./models/post");
 
 const app = express();
 
-mongoose.connect(
-  "mongodb+srv://timon9551:xu1kOTxtZ62pdg8q@cluster0.yvcgnyc.mongodb.net/node-angular"
-).then(() => {
-  console.log("mongodb connection established");
-}).catch(err => {
-  console.log('Connection error: ' + err.message);
-});
+mongoose
+  .connect(
+    "mongodb+srv://timon9551:xu1kOTxtZ62pdg8q@cluster0.yvcgnyc.mongodb.net/node-angular"
+  )
+  .then(() => {
+    console.log("mongodb connection established");
+  })
+  .catch((err) => {
+    console.log("Connection error: " + err.message);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,31 +36,24 @@ app.use((req, res, next) => {
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
   });
   post.save(post);
   res.status(201).json({
-    message: 'Post added successfully'
+    message: "Post added successfully",
   });
 });
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "adfsas124ds",
-      title: "First server-side post",
-      content: "This is coming from the server",
-    },
-    {
-      id: "kjui34o98",
-      title: "Second server-side post",
-      content: "This is coming from the server!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    posts: posts,
+app.get("/api/posts", (req, res, next) => {
+  Post.find()
+    .then((documents) => {
+      console.log(documents);
+      res.status(200).json({
+        message: "Posts fetched successfully",
+        posts: documents,
+      });
   });
+
 });
 
 module.exports = app;
