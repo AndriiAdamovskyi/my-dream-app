8d578fb9fd7c8302c2dfb8b1e16023552bbecd7a
Andrii Adamovskyi <timon9551@gmail.com>
2022-09-30 12:19:59 +0300

    + adding posts with ID

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -48,9 +48,13 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string; postId: string }>(
+        'http://localhost:3000/api/posts',
+        post
+      )
       .subscribe((responceData) => {
-        console.log(responceData.message);
+        const id = responceData.postId;
+        post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
@@ -60,7 +64,6 @@ export class PostsService {
     this.http
       .delete('http://localhost:3000/api/posts/' + postId)
       .subscribe(() => {
-        console.log('Delete post ' + postId);
         const updatedPosts = this.posts.filter((post) => post.id !== postId);
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
