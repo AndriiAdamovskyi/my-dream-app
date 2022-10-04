490d1fc4575fbf20e4c53a182d2d462fc1ed462f
Andrii Adamovskyi <timon9551@gmail.com>
2022-10-04 14:24:08 +0300

    added Server Side Upload

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const multer = require('multer')
+
 const Post = require("../models/post");
 
-const router = express.Router()
+const router = express.Router();
 
+const MIME_TYPE_MAP = {
+  'image/png': 'image/png',
+  'image/jpg': 'image/jpg',
+  'image/jpeg': 'image/jpeg'
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid MIME type: " + isValid.message);
+    if (isValid) {
+      error = null;
+    }
+    cb(error, 'backend/images');
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-')
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
 
 router.post("/api/posts");
 
-router.post("", (req, res, next) => {
+router.post("", multer(storage).single("image"), (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
