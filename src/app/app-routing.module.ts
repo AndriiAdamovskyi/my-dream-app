61899fb86fddb8e29e0355cfe89329b440f958a2
Andrii Adamovskyi <timon9551@gmail.com>
2022-10-07 19:33:15 +0300

    handling user input

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PostListComponent } from "./posts/post-list/post-list.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
@@ -14,7 +14,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms/';
 
-  @Component({
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
-  })
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  isLoading = false;
 
-  export class LoginComponent {
-    isLoading = false;
+  onLogin(form: NgForm) {
+    console.log(form.value);
   }
 
-
+}
