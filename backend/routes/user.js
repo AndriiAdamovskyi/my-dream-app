83dd29c2313a9b9b1994e30afc78e1bf6d8ea463
Andrii Adamovskyi <timon9551@gmail.com>
2022-10-17 15:28:35 +0300

    implementing spa authentication, added token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -31,5 +32,38 @@ router.post("/signup", (req, res, next) => {
       });
   }
 });
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: require.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          status: 401,
+          message: "User not found",
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password)
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed",
+          error: err
+        });
+      }
+      const token = jwt.sign({
+        email: user.email,
+        userId: user._id
+      }, 'secret_this_should_be_longer',
+      // additional security measurements
+        { expiresIn: "1h" } );
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed",
+        error: err,
+      });
+    })
+})
 
 module.exports = router;
