60991a1bbfc3161e69406a97b0b64e29d159bcdb
Andrii Adamovskyi <timon9551@gmail.com>
2022-10-17 14:58:07 +0300

    connected angular to the backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,24 +7,29 @@ const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
   //store in database encrypted password for security reasons
-  bcrypt.hash(req.params.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user.save()
-        .then(result => {
-          res.status(201).json({
-            status: 201,
-            message: "User created",
-            user: result,
-          });
-        })
-        .catch(err => {
-          res.status(500).json({ error: err.message });
+  if (req.body.password !== undefined) {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
         });
-  });
+        user
+          .save()
+          .then(result => {
+            res.status(201).json({
+              status: 201,
+              message: "User created",
+              user: result,
+            });
+          })
+          .catch(err => {
+            res.status(500).json({
+              error: err
+            });
+          });
+      });
+  }
 });
 
 module.exports = router;
