d1afea37464579c5d20f47f5745346747545f2e4
Andrii Adamovskyi <timon9551@gmail.com>
2022-10-20 14:37:45 +0300

    misspelling

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -18,7 +18,7 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        let errorMessage = "An uknown error occured";
+        let errorMessage = "An unknown error occurred";
         if (error.error.message) {
           errorMessage = error.error.message;
         }
