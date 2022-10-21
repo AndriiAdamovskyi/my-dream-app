ab26e6f2c6e1cdcf4bd398ba87cfd1c94e232f4f
Andrii Adamovskyi <timon9551@gmail.com>
2022-10-21 12:27:27 +0300

    used Controllers

---
+++ backend/controllers/user.js
@@ -0,0 +1,70 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+exports.createUser = (req, res, next) => {
+  //store in database encrypted password for security reasons
+  if (req.body.password !== undefined) {
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then((result) => {
+          res.status(201).json({
+            status: 201,
+            message: "User created",
+            user: result,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            message: "Invalid authentication credentials",
+          });
+        });
+    });
+  }
+};
+
+exports.loginUser = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          status: 401,
+          message: "User not found",
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed",
+          error: err,
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        // additional security measurements
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        //1 hour in seconds
+        expiresIn: 3600,
+        userId: fetchedUser._id,
+      });
+    })
+    .catch((err) => {
+      return res.status(401).json({
+        message: "Invalid authentication credentials",
+      });
+    });
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,75 +1,11 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
-const User = require("../models/user");
+const UserController = require("../controllers/user")
 
 const router = express.Router();
 
-router.post("/signup", (req, res, next) => {
-  //store in database encrypted password for security reasons
-  if (req.body.password !== undefined) {
-    bcrypt.hash(req.body.password, 10).then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then((result) => {
-          res.status(201).json({
-            status: 201,
-            message: "User created",
-            user: result,
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            message: "Invalid authentication credentials",
-          });
-        });
-    });
-  }
-});
+router.post("/signup", UserController.createUser);
 
-router.post("/login", (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({
-          status: 401,
-          message: "User not found",
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then((result) => {
-      if (!result) {
-        return res.status(401).json({
-          message: "Auth failed",
-          error: err,
-        });
-      }
-      const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        "secret_this_should_be_longer",
-        // additional security measurements
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token: token,
-        //1 hour in seconds
-        expiresIn: 3600,
-        userId: fetchedUser._id,
-      });
-    })
-    .catch((err) => {
-      return res.status(401).json({
-        message: "Invalid authentication credentials",
-      });
-    });
-});
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
